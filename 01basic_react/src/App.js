82c464d67f496e9992890061b4e674c1f9d6f00a
adityaagrawal06 <adityaagra06@gmail.com>
2026-02-12 01:03:28 +0530

    2nd lec

---
+++ 01basic_react/src/App.js
@@ -0,0 +1,9 @@
+
+function App() {
+  return (
+    <h1>chai aur react | Aditya Agrawal
+    </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function App() {
+  
+
+  return (
+        <h1>Chai aur react with vite | Aditya Agrawal</h1>
+  )
+}
+
+export default App
