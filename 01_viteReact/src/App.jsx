bd2896955a94331be361509a78c3b305e21e266a
Aditya Agrawal <adityaagra06@gmail.com>
2026-05-24 00:03:19 +0530

    lec 4 commplete

--- 01_viteReact/src/App.jsx
+++ 01_viteReact/src/App.jsx
@@ -4,15 +4,15 @@ import Chai from "./chai"
 
 function App() {
   
-
+  
+  const username="Aditya"
   return (
   //  <h1>Chai aur React with vite</h1> //Ek hi element return karna hota hai
   // <Chai/>
 
   //Method 2 Fragment
-
   <>
-  <h1>Chai aur React with vite</h1> 
+  <h1>Chai aur React with vite || {username}</h1>   // { } is called evaluation brackets, it is used to evaluate JavaScript expressions inside JSX. It allows us to embed dynamic content, such as variables, functions, or any valid JavaScript expression, within our JSX code. In this case, we are using it to display the value of the username variable within the h1 element.
    <Chai/>
   </>
   )
--- 01_viteReact/src/main.jsx
+++ 01_viteReact/src/main.jsx
@@ -1,33 +1,35 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-// import App from './App.jsx'
+import App from './App.jsx'
 
 
-// function Myapp(){
-//   return(
-//     <div>
-//       <h1>Hello World</h1>
-//     </div>
-//   )
-// }
+// // // function Myapp(){
+// // //   return(
+// // //     <div>
+// // //       <h1>Hello World</h1>
+// // //     </div>
+// // //   )
+// // // }
 
-// const ReactElement={
-//     type:'a',
-//     props:{
-//         href:'https://www.google.com',
-//         target:'_blank',
-//     },
-//     children:'Click me to visit Google'
-// }
-
+// // // const ReactElement={
+// // //     type:'a',
+// // //     props:{
+// // //         href:'https://www.google.com',
+// // //         target:'_blank',
+// // //     },
+// // //     children:'Click me to visit Google'
+// // // }
+const anotherUser= "Aditya"
 const reactElement=React.createElement(
   'a',
   {
     href:'https://www.google.com',
     target:'_blank'
   }, 
-  'Click me to visit Google'
+  'Click me to visit Google',
+  anotherUser
 ); 
 
 const anotherElement= (
@@ -36,9 +38,13 @@ const anotherElement= (
 
 createRoot(document.getElementById('root')).render(
   
-  //  <ReactElement /> //will not occus for 1st as it is a object and not a component, it will throw an error because React expects a component or a string for HTML elements, but here we are passing an object which is not valid. To render this object, we would need to create a custom render function that can handle this type of object and convert it into a valid React element or HTML element.
-  // anotherElement //correct
-  anotherElement 
+//   //  <ReactElement /> //will not occus for 1st as it is a object and not a component, it will throw an error because React expects a component or a string for HTML elements, but here we are passing an object which is not valid. To render this object, we would need to create a custom render function that can handle this type of object and convert it into a valid React element or HTML element.
+//   // anotherElement //correct
+  reactElement  //correct
+
+  // <App />
 
 
 )
+
+
