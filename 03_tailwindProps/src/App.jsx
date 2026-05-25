d94c8dfd7a3c961c513e666ed9e5a90e535e917c
Aditya Agrawal <adityaagra06@gmail.com>
2026-05-25 23:58:19 +0530

    lec 7 props and tailwind

---
+++ 03_tailwindProps/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    name: "Adity",
+    channel: "bored-ape"
+  }
+  let myArr = [1, 2, 3, 4, 5]
+  let arr= [1, 2, 3]
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-green-500 p-4 rounded-xl">Bg Tailwind</h1>
+      <Card channel={myObj.channel} name={myObj.name} myArray={myArr} /> // passing props to card component
+      <Card channel="adiiiiii" name="Adityaaaaa" myArray={arr} />
+      <Card channel="adiiiiii"  /> //if do not pass a prop then it will be undefined in the card component
+
+      
+    </>
+  )
+}
+
+export default App
+
